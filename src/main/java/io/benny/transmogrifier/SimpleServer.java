93a387c7485a9ba934f9f8a1e2f28a36c1b3fee0
Benny Zlotnik <bzlotnik@redhat.com>
2017-01-28 12:19:59 +0200

    SimpleServer version

---
+++ src/main/java/io/benny/transmogrifier/SimpleServer.java
@@ -0,0 +1,35 @@
+package io.benny.transmogrifier;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by benny on 1/28/17.
+ */
+public class SimpleServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(8080);
+
+        while (true) {
+            Socket s = ss.accept();
+            InputStream in = s.getInputStream();
+            OutputStream out = s.getOutputStream();
+            int data;
+
+            while ((data = in.read()) != -1) {
+                out.write(transmogrify(data));
+            }
+
+            out.close();
+            in.close();
+            s.close();
+        }
+    }
+
+    private static int transmogrify(int data) {
+        return Character.isLetter(data) ? data ^ ' ' : data;
+    }
+}
